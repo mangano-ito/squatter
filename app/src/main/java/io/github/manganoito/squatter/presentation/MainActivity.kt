49b7db404ba0d8d3041085edf78b199547e13f73
mangano-ito <mangano-ito@hatena.ne.jp>
2025-01-01 19:31:31 +0900

    refactor app root with NavHost

--- app/src/main/java/io/github/manganoito/squatter/presentation/MainActivity.kt
+++ app/src/main/java/io/github/manganoito/squatter/presentation/MainActivity.kt
@@ -8,23 +8,18 @@ package io.github.manganoito.squatter.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
 import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.Text
 import androidx.wear.compose.material.TimeText
-import androidx.wear.tooling.preview.devices.WearDevices
 import dagger.hilt.android.AndroidEntryPoint
-import io.github.manganoito.squatter.R
+import io.github.manganoito.squatter.navigation.SquatterNavHost
 import io.github.manganoito.squatter.presentation.theme.SquatterTheme
 
 @AndroidEntryPoint
@@ -37,14 +32,15 @@ class MainActivity : ComponentActivity() {
         setTheme(android.R.style.Theme_DeviceDefault)
 
         setContent {
-            WearApp("Android")
+            WearApp()
         }
     }
 }
 
 @Composable
-fun WearApp(greetingName: String) {
+fun WearApp() {
     SquatterTheme {
+        val navController = rememberNavController()
         Box(
             modifier = Modifier
                 .fillMaxSize()
@@ -52,23 +48,7 @@ fun WearApp(greetingName: String) {
             contentAlignment = Alignment.Center
         ) {
             TimeText()
-            Greeting(greetingName = greetingName)
+            SquatterNavHost(navController = navController)
         }
     }
-}
-
-@Composable
-fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
-    )
-}
-
-@Preview(device = WearDevices.SMALL_ROUND, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    WearApp("Preview Android")
 }
