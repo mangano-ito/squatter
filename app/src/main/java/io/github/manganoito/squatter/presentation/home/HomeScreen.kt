c99a032f5132ae5245e8de15021fd54c89c6d875
mangano-ito <mangano-ito@hatena.ne.jp>
2025-01-01 19:27:21 +0900

    add home screen

---
+++ app/src/main/java/io/github/manganoito/squatter/presentation/home/HomeScreen.kt
@@ -0,0 +1,10 @@
+package io.github.manganoito.squatter.presentation.home
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.Text
+
+@Composable
+fun HomeScreen() {
+    // TODO
+    Text("Home")
+}
