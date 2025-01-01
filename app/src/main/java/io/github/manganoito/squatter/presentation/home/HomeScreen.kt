5c79bd841973c992c6d777544d7c9356d412c286
mangano-ito <mangano-ito@hatena.ne.jp>
2025-01-01 20:00:22 +0900

    add stateless component and preview

--- app/src/main/java/io/github/manganoito/squatter/presentation/home/HomeScreen.kt
+++ app/src/main/java/io/github/manganoito/squatter/presentation/home/HomeScreen.kt
@@ -1,13 +1,34 @@
 package io.github.manganoito.squatter.presentation.home
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.wear.compose.material.Text
+import io.github.manganoito.squatter.presentation.theme.SquatterTheme
 
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel(),
 ) {
+    HomeScreen(
+        uiState = viewModel.uiState,
+    )
+}
+
+@Composable
+private fun HomeScreen(
+    uiState: HomeUiState,
+) {
     // TODO
     Text("Home")
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewHomeScreen() {
+    SquatterTheme {
+        HomeScreen(
+            uiState = HomeUiState.Initial,
+        )
+    }
 }
