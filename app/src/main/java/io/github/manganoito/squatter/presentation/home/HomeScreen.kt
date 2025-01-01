debf619db8a6906346350487dbbcc1347a3ca443
mangano-ito <mangano-ito@hatena.ne.jp>
2025-01-01 19:40:40 +0900

    add home view model and injection

--- app/src/main/java/io/github/manganoito/squatter/presentation/home/HomeScreen.kt
+++ app/src/main/java/io/github/manganoito/squatter/presentation/home/HomeScreen.kt
@@ -1,10 +1,13 @@
 package io.github.manganoito.squatter.presentation.home
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.wear.compose.material.Text
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    viewModel: HomeViewModel = hiltViewModel(),
+) {
     // TODO
     Text("Home")
 }
