178eaa13e32099f148d38d8b067f8b517bf7efd3
mangano-ito <mangano-ito@hatena.ne.jp>
2025-01-06 16:38:27 +0900

    inject accelerometer deps

---
+++ app/src/main/java/io/github/manganoito/squatter/logic/accelerometer/AndroidAccelerometerModule.kt
@@ -0,0 +1,20 @@
+package io.github.manganoito.squatter.logic.accelerometer
+
+import android.content.Context
+import android.hardware.SensorManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class AndroidAccelerometerModule {
+    @Provides
+    @Singleton
+    fun provideSensorManager(@ApplicationContext context: Context): SensorManager {
+        return context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+}
