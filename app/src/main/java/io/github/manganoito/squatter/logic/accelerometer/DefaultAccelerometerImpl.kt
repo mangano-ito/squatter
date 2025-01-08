6f6651c0ca8c716bcd49f5f3d5e47b636689d79b
mangano-ito <mangano-ito@hatena.ne.jp>
2025-01-09 01:38:24 +0900

    watch gravity sensor values

--- app/src/main/java/io/github/manganoito/squatter/logic/accelerometer/DefaultAccelerometerImpl.kt
+++ app/src/main/java/io/github/manganoito/squatter/logic/accelerometer/DefaultAccelerometerImpl.kt
@@ -7,14 +7,30 @@ import android.hardware.SensorManager
 import android.util.Log
 import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class DefaultAccelerometerImpl @Inject constructor(
     private val sensorManager: SensorManager,
 ) : Accelerometer, SensorEventListener {
     private lateinit var accelerationSensor: Sensor
+    private lateinit var gravitySensor: Sensor
 
     private val _accelerationVector = MutableStateFlow<FloatArray>(floatArrayOf(0f, 0f, 0f))
+    private val _unitGravityVector = MutableStateFlow<FloatArray>(floatArrayOf(0f, 0f, 1f))
     override val acceleration = MutableStateFlow<Float>(0f)
+
+    private fun FloatArray.calcNorm(): Float {
+        return sqrt(map { it.pow(2) }.sum())
+    }
+
+    private operator fun FloatArray.div(other: Float): FloatArray {
+        return map { it / other }.toFloatArray()
+    }
+
+    private fun FloatArray.toUnitVector(): FloatArray {
+        return this / calcNorm()
+    }
 
     override suspend fun report() {
         TODO()
@@ -23,8 +39,12 @@ class DefaultAccelerometerImpl @Inject constructor(
     override fun listen() {
         sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)?.let {
             this.accelerationSensor = it
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_FASTEST)
         }
-        sensorManager.registerListener(this, accelerationSensor, SensorManager.SENSOR_DELAY_FASTEST)
+        sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)?.let {
+            this.gravitySensor = it
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_FASTEST)
+        }
     }
 
     override fun unlisten() {
@@ -40,6 +60,10 @@ class DefaultAccelerometerImpl @Inject constructor(
                     "[${event.values[0]}, ${event.values[1]}, ${event.values[2]}]"
                 )
                 _accelerationVector.value = event.values
+            }
+
+            gravitySensor -> {
+                _unitGravityVector.value = event.values.toUnitVector()
             }
         }
     }
